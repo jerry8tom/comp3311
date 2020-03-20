898fce0e683eb37a99794d9e18bbfda4fd70348c
= <mukul02015@gmail.com>
2020-03-20 16:51:05 +1100

    final

--- a1.sql
+++ a1.sql
@@ -26,7 +26,7 @@ select DISTINCT ON (t.brand)t.brand, t.id, t.pno, t.premium
 FROM tmp t 
 ORDER BY t.brand, t.premium DESC;
 
-CREATE OR REPLACE VIEW Q2 (brand, id, pno, premium) AS
+CREATE OR REPLACE VIEW Q2 (brand, car_id, pno, premium) AS
 select * from tmp2 t
 ORDER BY t.brand, t.id, t.pno ASC;
 
@@ -45,30 +45,29 @@ CREATE OR REPLACE VIEW Q3 (pid, firstname, lastname) AS
 select p.pid, p.firstname, p.lastname 
 FROM person p
 JOIN tmp3 s ON (s.pid = p.pid)
-WHERE s.wdate - CURRENT_DATE > 365 OR s.wdate IS NULL;
+WHERE s.wdate - CURRENT_DATE > 365 OR s.wdate IS NULL
+ORDER BY p.pid ASC;
 
 
 -- Q4
 
 
--- TODO : what if we have multiple people living 
--- in the same suburb
-
-select UPPER(p.suburb), COUNT(po.pno)        
-FROM person p                      
-JOIN client c ON (p.pid = c.pid AND p.state = 'NSW')   
+CREATE OR REPLACE VIEW Q4 (suburb, npolicies) AS
+select UPPER(p.suburb), SUM(COUNT(po.pno)) OVER (PARTITION BY p.suburb) AS npol
+FROM person p
+JOIN client c ON (p.pid = c.pid AND p.state = 'NSW')
 JOIN insured_by i ON (i.cid = c.cid)  
 JOIN policy po ON(po.pno = i.pno)
 GROUP BY p.suburb
-ORDER BY COUNT(po.pno), p.suburb;
+ORDER BY npol, p.suburb;
 
 
 -- Q5
 
 
-create or replace view Q5(pno, pname, pid, firstname, lastname) as
+CREATE OR REPLACE VIEW Q5(pno, pname, pid, firstname, lastname) AS
 
-select DISTINCT(p.pno), p.pname, pe.pid, pe.firstname, pe.lastname
+SELECT DISTINCT(p.pno), p.pname, pe.pid, pe.firstname, pe.lastname
 FROM person pe
     JOIN staff s ON (pe.pid = s.pid)
     JOIN policy p ON (p.sid = s.sid)                --sold by
@@ -80,7 +79,7 @@ ORDER BY p.pno ASC;
 -- Q6
 
 
-create or replace view tmp6(pid, name, brand) as
+CREATE OR REPLACE VIEW tmp6(pid, name, brand) AS
 
 SELECT p.pid , p.firstname ||' '|| p.lastname as name, i.brand
 FROM policy po 
@@ -89,7 +88,9 @@ JOIN staff s ON (s.sid = po.sid)
 JOIN person p ON (p.pid = s.pid)
 GROUP BY p.pid, i.brand;
 
-select t.pid, t.name, t.brand
+CREATE OR REPLACE VIEW Q6 (pid, name, brand) AS
+
+SELECT t.pid, t.name, t.brand
 FROM tmp6 t 
 JOIN (select t2.pid FROM tmp6 t2 GROUP BY t2.pid 
 HAVING COUNT(t2.brand) = 1)
@@ -101,10 +102,10 @@ ORDER BY t.pid ASC;
 -- Q7
 
 
-create or replace view total(total) as
-select COUNT (DISTINCT brand) FROM insured_item;
+CREATE OR REPLACE VIEW total(total) AS
+SELECT COUNT (DISTINCT brand) FROM insured_item;
 
-create or replace view tmp7(pid, name, cnt) as
+CREATE OR REPLACE VIEW tmp7(pid, name, cnt) AS
 SELECT p.pid, p.firstname ||' '|| p.lastname as name, COUNT (DISTINCT ii.brand)
 FROM person p 
 JOIN client c ON (p.pid = c.pid)
@@ -115,6 +116,7 @@ GROUP BY p.pid
 ORDER BY p.pid ASC;
 
 CREATE OR REPLACE VIEW Q7 (pid, name) AS
+
 select t.pid, t.name
 FROM tmp7 t, total tot
 WHERE t.cnt = tot.total
@@ -127,14 +129,14 @@ ORDER BY t.pid ASC;
 -- Q9
 
 
-CREATE OR REPLACE VIEW q9 (rid, coid, pno, status, expirydate, rate) AS
-select rr.rid, rr.coid, p.pno, p.status, p.expirydate, rr.rate
+CREATE OR REPLACE VIEW Q9 (rid, coid, pno, status, expirydate, rate) AS
+SELECT rr.rid, rr.coid, p.pno, p.status, p.expirydate, rr.rate
 FROM rating_record rr
 JOIN coverage c ON (c.coid = rr.coid)
 JOIN policy p ON (p.pno = c.pno)
 WHERE p.status = 'E' AND p.expirydate > CURRENT_DATE AND effectivedate <= CURRENT_DATE; 
 
-create or replace function ratechange(Adj integer) returns integer
+CREATE OR REPLACE FUNCTION ratechange(Adj integer) RETURNS INTEGER
 AS $$
 declare ret_val integer;
 BEGIN
@@ -153,8 +155,37 @@ $$ LANGUAGE plpgsql;
 
 -- Q10
 
-CREATE FUNCTION q10 () RETURNS trigger AS $trig_func$
-    BEGIN 
-    IF (TG_PO = 'UPDATE') THEN 
+
+-- all the staff that own a policy
+CREATE OR REPLACE VIEW imposs (pno) AS
+select po.pno
+FROM policy po
+JOIN insured_by i ON (i.pno = po.pno)
+JOIN client c ON (c.cid = i.cid)
+JOIN person p ON (p.pid = c.pid)
+JOIN staff s ON (s.pid = p.pid);
+
+-- policies which are not owned by staff
+CREATE OR REPLACE VIEW poss (pno) AS
+select p.pno
+FROM policy p
+WHERE p.pno NOT IN (select * from imposs);
+
+
+CREATE FUNCTION q10 () RETURNS trigger AS $$
+BEGIN
+    IF EXISTS (SELECT * FROM poss WHERE poss.pno = OLD.pno) THEN
+    UPDATE expirydate
+    SET expirydate = expirydate + 30
+    WHERE NEW.status = 'E' AND OLD.pno != NEW.pno;
+    END IF;
+return NEW;
+END;
+$$ language plpgsql;
+
+
+CREATE TRIGGER trig 
+AFTER UPDATE ON policy 
+FOR EACH ROW EXECUTE PROCEDURE q10();
 
 
